635b37032304a009a666ac4662033b7ccf307fe1
Joshua Nelson <joshuaknelson1202@icloud.com>
2026-06-06 09:37:19 -0500

    added webserver structure

---
+++ webserver.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+  "net/http"
+
+  "github.com/gin-gonic/gin"
+)
+
+func getting(c *gin.Context) {
+  c.HTML(http.StatusOK, "dashboard/main.tmpl", gin.H{"title": "Josh's Webpage"})
+}
+
+func posting(c *gin.Context) {
+  c.JSON(http.StatusOK, gin.H{"method": "POST"})
+}
+
+func putting(c *gin.Context) {
+  c.JSON(http.StatusOK, gin.H{"method": "PUT"})
+}
+
+func deleting(c *gin.Context) {
+  c.JSON(http.StatusOK, gin.H{"method": "DELETE"})
+}
+
+func patching(c *gin.Context) {
+  c.JSON(http.StatusOK, gin.H{"method": "PATCH"})
+}
+
+func head(c *gin.Context) {
+  c.Status(http.StatusOK)
+}
+
+func options(c *gin.Context) {
+  c.Status(http.StatusOK)
+}
+
+func main() {
+  // Creates a gin router with default middleware:
+  // logger and recovery (crash-free) middleware
+  	router := gin.Default()
+	router.LoadHTMLGlob("templates/**/*")
+	router.GET("/", getting)
+	router.POST("/somePost", posting)
+	router.PUT("/somePut", putting)
+	router.DELETE("/someDelete", deleting)
+	router.PATCH("/somePatch", patching)
+	router.HEAD("/someHead", head)
+	router.OPTIONS("/someOptions", options)
+
+	// By default it serves on :8080 unless a
+	// PORT environment variable was defined.
+	PORT := ":8080"
+	router.Run(PORT)
+}
